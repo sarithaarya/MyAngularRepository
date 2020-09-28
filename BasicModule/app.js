20da8428c96db22715872e189d8f523b7299254a
Saritha Arya <arya.saritha@gmail.com>
2020-09-28 22:51:07 +0530

    code cleanup

--- BasicModule/app.js
+++ BasicModule/app.js
@@ -1,57 +1,30 @@
- //(function functionName() {
-//   //'use strict';
-//
-// angular.module('myFirstApp', [])
-// .controller("MyFirstController", function($scope){
-//
-//   $scope.searchText = "";
-//     $scope.check = function () {
-//       console.log("You typed '" + this.searchText + "'"); // used 'this' instead of $scope
-//     }
-// });
-//})();
 
 (function functionName() {
   'use strict';
 /*NameCalculator*/
+  angular.module('NameCalculator',[])
 
+  .controller('NameCalculatorController', function($scope){
 
-//   angular.module('NameCalculator',[])
-//
-//   .controller('NameCalculatorController', function($scope){
-//
-// $scope.name="";
-// $scope.totalvalue =0;
-// $scope.displayNumeric = function(){
-//
-//   var totalNameValue =calculateNumericForString($scope.name);
-//   $scope.totalvalue=totalNameValue;
-// }
-//
-// function calculateNumericForString(string)
-// {
-//   var totalStringValue = 0;
-//   for(var i=0; i<string.length; i++)
-//   {
-//     totalStringValue += string.charCodeAt(i);
-//   }
-//
-//   return totalStringValue;
-// }
-//   });
+$scope.name="";
+$scope.totalvalue =0;
+$scope.displayNumeric = function(){
 
-angular.module('DIapp',[])
-.controller('DIController',DIController);
-
-function DIController($scope, $filter)
-{
-  $scope.name="Saritha";
-
-  $scope.upper=function(){
-var upCase = $filter('uppercase');
-$scope.name=upCase($scope.name);
-  };
-
+  var totalNameValue =calculateNumericForString($scope.name);
+  $scope.totalvalue=totalNameValue;
 }
+
+function calculateNumericForString(string)
+{
+  var totalStringValue = 0;
+  for(var i=0; i<string.length; i++)
+  {
+    totalStringValue += string.charCodeAt(i);
+  }
+
+  return totalStringValue;
+}
+  });
+
 
   })();
